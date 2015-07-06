3bf6a32ddca25ff128e7e9480cfb0e4c9901fd57
brianr7400 <brianr7400@gmail.com>
2015-07-06 15:57:46 -0400

    Added the entire project

---
+++ Click Paster Winform/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Click_Paster_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.recordButton = new System.Windows.Forms.Button();
+            this.playButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.eventBox = new System.Windows.Forms.TextBox();
+            this.playTimer = new System.Windows.Forms.Timer(this.components);
+            this.intervalBox = new System.Windows.Forms.TextBox();
+            this.loopBox = new System.Windows.Forms.CheckBox();
+            this.looptimesBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // recordButton
+            // 
+            this.recordButton.BackColor = System.Drawing.Color.Red;
+            this.recordButton.Location = new System.Drawing.Point(13, 13);
+            this.recordButton.Name = "recordButton";
+            this.recordButton.Size = new System.Drawing.Size(75, 23);
+            this.recordButton.TabIndex = 0;
+            this.recordButton.Text = "Record";
+            this.recordButton.UseVisualStyleBackColor = false;
+            this.recordButton.Click += new System.EventHandler(this.recordButton_Click);
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.playButton.Location = new System.Drawing.Point(13, 43);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 23);
+            this.playButton.TabIndex = 1;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.BackColor = System.Drawing.Color.Black;
+            this.stopButton.ForeColor = System.Drawing.Color.White;
+            this.stopButton.Location = new System.Drawing.Point(13, 73);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // eventBox
+            // 
+            this.eventBox.Location = new System.Drawing.Point(106, 13);
+            this.eventBox.Multiline = true;
+            this.eventBox.Name = "eventBox";
+            this.eventBox.ReadOnly = true;
+            this.eventBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.eventBox.Size = new System.Drawing.Size(223, 160);
+            this.eventBox.TabIndex = 3;
+            this.eventBox.Text = "EVENTS:";
+            // 
+            // playTimer
+            // 
+            this.playTimer.Interval = 200;
+            this.playTimer.Tick += new System.EventHandler(this.playTimer_Tick);
+            // 
+            // intervalBox
+            // 
+            this.intervalBox.Location = new System.Drawing.Point(13, 102);
+            this.intervalBox.Name = "intervalBox";
+            this.intervalBox.Size = new System.Drawing.Size(75, 20);
+            this.intervalBox.TabIndex = 4;
+            // 
+            // loopBox
+            // 
+            this.loopBox.AutoSize = true;
+            this.loopBox.Location = new System.Drawing.Point(13, 131);
+            this.loopBox.Name = "loopBox";
+            this.loopBox.Size = new System.Drawing.Size(50, 17);
+            this.loopBox.TabIndex = 5;
+            this.loopBox.Text = "Loop";
+            this.loopBox.UseVisualStyleBackColor = true;
+            // 
+            // looptimesBox
+            // 
+            this.looptimesBox.Location = new System.Drawing.Point(13, 153);
+            this.looptimesBox.Name = "looptimesBox";
+            this.looptimesBox.Size = new System.Drawing.Size(75, 20);
+            this.looptimesBox.TabIndex = 6;
+            this.looptimesBox.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(341, 185);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.looptimesBox);
+            this.Controls.Add(this.loopBox);
+            this.Controls.Add(this.intervalBox);
+            this.Controls.Add(this.eventBox);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.recordButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Mouse Player";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button recordButton;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.TextBox eventBox;
+        private System.Windows.Forms.Timer playTimer;
+        private System.Windows.Forms.TextBox intervalBox;
+        private System.Windows.Forms.CheckBox loopBox;
+        private System.Windows.Forms.TextBox looptimesBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Click Paster Winform/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Click_Paster_WinForm
+{
+    public partial class Form1 : Form
+    {
+        #region Declare Variables
+        //if program is 'recording' input
+        bool recording = false;
+        //lists with the cordinated of the mouse positon
+        List<int> xloc = new List<int>();
+        List<int> yloc = new List<int>();
+        //List of the events that happen during playback
+        List<string> playbackEvents = new List<string>();
+        //Variable that is used to show what event is currently being done
+        int currentevent = 0;
+        //Variable that counts which position in the location of the
+        int currentLocation = 0;
+        //checks how many times its looped
+        double looptimes = 0;
+        #endregion
+        #region Import Mouse
+        //Imports the ability to click mouse
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        public const int MOUSEEVENTF_RIGHTUP = 0x10;
+        #endregion
+        //My Methods
+        #region Methods
+        //Checks which event is next and what function to use
+        void DoEvent(string cEvent)
+        {
+            //Checks if the current event is Click or Move Mouse
+            if (cEvent == "Click")
+            {
+                DoMouseClick();
+            }
+            if (cEvent == "Move Mouse")
+            {
+                MoveMouse(currentLocation);
+                currentLocation++;
+            }
+            if (cEvent == "Right Click")
+            {
+                DoRightMouseClick();
+            }
+            //Adds 1 to the amount of times looped
+            looptimes++;
+            label1.Text = Math.Floor(looptimes/playbackEvents.Count()).ToString();
+            if (looptimes > Convert.ToInt32(looptimesBox.Text)*playbackEvents.Count())
+            {
+                loopBox.Checked = false;
+                looptimes = 0;
+                
+            }
+        }
+        //Moves mouse to next location in array
+        void MoveMouse(int cEvent)
+        {
+            Cursor.Position = new Point(xloc[cEvent], yloc[cEvent]);
+        }
+        //Click Mouse Method
+        public void DoMouseClick()
+        {
+            //Call the imported function with the cursor's current position
+            int X = Cursor.Position.X;
+            int Y = Cursor.Position.Y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+        }
+        //Right Click Mouse
+        public void DoRightMouseClick()
+        {
+            //Call the imported function with the cursor's current position
+            int X = Cursor.Position.X;
+            int Y = Cursor.Position.Y;
+            mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, X, Y, 0, 0);
+        }
+        #endregion
+        //Checks if keys are pressed if the record timer is running
+        #region CheckKeyPressed
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Q)
+            {
+                if (loopBox.Checked == true)
+                {
+                    loopBox.Checked = false;
+                }
+                else if (loopBox.Checked == false)
+                {
+                    loopBox.Checked = true;
+                }
+            }
+            if (keyData == Keys.C && recording == true)
+            {
+                //Gets the mouse cursor location and puts them in a variable
+                int X = Cursor.Position.X;
+                int Y = Cursor.Position.Y;
+
+                //Adds to the event list that it should click
+                playbackEvents.Add("Click");
+
+                //Adds the event to the textbox
+                string eventadd = string.Format("\r\nClick Mouse at current Location");
+                eventBox.AppendText(eventadd);
+                return true;
+            }
+            if (keyData == Keys.M && recording == true)
+            {
+                //Gets the mouse cursor location and puts them in a variable
+                int X = Cursor.Position.X;
+                int Y = Cursor.Position.Y;
+
+                //Adds to the event that it should move the mouse to the specified location
+                playbackEvents.Add("Move Mouse");
+
+                //Adds the cursor position to the cordinate arrays;
+                xloc.Add(X);
+                yloc.Add(Y);
+
+                //Adds the event to the textbox
+                string eventadd = string.Format("\r\nMove Mouse to ( {0},{1} )", X.ToString(), Y.ToString());
+                eventBox.AppendText(eventadd);
+                return true;
+            }
+            if (keyData == Keys.X && recording == true)
+            {
+                //Gets the mouse cursor location and puts them in a variable
+                int X = Cursor.Position.X;
+                int Y = Cursor.Position.Y;
+
+                //Adds to the event list that it should click
+                playbackEvents.Add("Right Click");
+
+                //Adds the event to the textbox
+                string eventadd = string.Format("\r\nRight Click Mouse at current Location");
+                eventBox.AppendText(eventadd);
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Button Click Events
+        #region ButtonClick
+        private void recordButton_Click(object sender, EventArgs e)
+        {
+            recording = true;
+            //Resets lists
+            xloc.Clear();
+            yloc.Clear();
+            playbackEvents.Clear();
+            eventBox.Text = "EVENTS:";
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            playTimer.Start();
+            if (intervalBox.Text != "")
+            {
+                playTimer.Interval = Convert.ToInt32(intervalBox.Text);
+            }
+            looptimes = 0;
+            
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            //Stops both timers (play & record)
+            recording = false;
+            playTimer.Stop();
+            //Resets the variables
+            currentevent = 0;
+            currentLocation = 0;
+        }
+        #endregion
+        //Timers
+        #region Timers
+        private void playTimer_Tick(object sender, EventArgs e)
+        {
+            
+            //Checks if the currentevent is larger then the list/array of events
+            if (currentevent >= playbackEvents.Count())
+            {
+                currentLocation = 0;
+                currentevent = 0;
+                if (loopBox.Checked == false)
+                {
+                    playTimer.Stop();
+                }
+            }
+            else
+            {
+                //Does the next action in the event list
+                DoEvent(playbackEvents[currentevent]);
+
+                //Moves to the next event
+                currentevent++;
+            }
+            
+        }
+        #endregion
+    }
+}
